a470fbea155f4e2181f422a2acc8939ae641dfac
lavanyauj <86716651+lavanyauj@users.noreply.github.com>
2021-07-14 20:06:18 +0530

    Add files via upload

---
+++ src/FormSuccess.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Successfully logged in.</h1>
+    </div>
+  );
+};
+
+export default FormSuccess;
